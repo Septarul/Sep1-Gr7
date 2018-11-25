bb4114d972329b282b0586d61139b5db5f23d8aa
Radu <raducandea99@gmail.com>
2018-11-25 16:06:42 +0100

    training and employee

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -12,11 +12,29 @@ public class Employee
 
    public Employee(name name, String id)
    {
-      this.id=id;
+      this.id = id;
       this.name = name;
       this.freeDays = null;
       this.pref = null;
-      this.train= new ArrayList<>();
+      this.train = new ArrayList<>();
+   }
+
+   public ArrayList<Training> getCompletedTraining()
+   {
+      ArrayList<Training> completed = new ArrayList<>();
+      for (int i = 0; i < train.size(); i++)
+      {
+         if (train.get(i).getStatus() == 3)
+         {
+            completed.add(train.get(i));
+         }
+      }
+      return completed;
+   }
+
+   public ArrayList<Training> getTraining()
+   {
+      return train;
    }
 
    public name getName()
@@ -27,11 +45,6 @@ public class Employee
    public void setName(name name)
    {
       this.name = name;
-   }
-
-   public ArrayList<Training> getTraining()
-   {
-      return train;
    }
 
    public FreeDays getFreeDays()
--- src/model/Training.java
+++ src/model/Training.java
@@ -11,26 +11,45 @@ public class Training
       this.task = task;
    }
 
-   
+   public void train(String task)
+   {
+      this.status = 2;
+      this.task = task;
+   }
+
+   public void completeTraining(String task)
+   {
+      this.task = task;
+      this.status = 3;
+   }
    public int getStatus()
    {
       return status;
    }
-
-   public void setStatus(int status)
+   public String getStatusString()
    {
-      this.status = status;
+      if (status == 1)
+      {
+         return "Untrained";
+      }
+      else if (status == 2)
+      {
+         return "In training";
+      }
+      else
+      {
+         return "Trained";
+      }
+   }
+
+   public void setUntrained(String task)
+   {
+      this.task = task;
+      this.status = 1;
    }
 
    public String getTask()
    {
       return task;
    }
-
-   public void setTask(String task)
-   {
-      this.task = task;
-   }
-   
-
 }
